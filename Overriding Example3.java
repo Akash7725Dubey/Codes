fea2af94f068802a47198b1615515f677807afa4
Akash7725Dubey <140379436+Akash7725Dubey@users.noreply.github.com>
2023-07-27 10:49:53 +0530

    Update and rename Maining.java to Overriding Example3.java

---
+++ Overriding Example3.java
@@ -0,0 +1,36 @@
+class Tv
+{
+    public void SwitchOn()
+    {
+        System.out.println("Tv is Switched on");
+    }
+    public void ChangeChannel()
+    {
+        System.out.println("TV Channel is changed");
+    }
+}
+class SmartTv extends Tv
+{
+    public void SwitchOn()
+    {
+        System.out.println("Smart Tv is Switched On");
+    }
+    public void ChangeChannel()
+    {
+        System.out.println("Smart Tv channel is Changed");
+    }
+    public void Browse()
+    {
+        System.out.println("Smart Tv is Browsed");
+    }
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        Tv t=new SmartTv();
+        t.ChangeChannel();
+        t.SwitchOn();
+        
+    }
+}

4709c1d147cb94e4d7f875d6735706751c208493
Akash7725Dubey <140379436+Akash7725Dubey@users.noreply.github.com>
2023-07-29 10:42:36 +0530

    Add files via upload

---
+++ try and catch.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        int a=10,b=0,c;
+        try {
+            c = a / b;
+            System.out.println(c);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("Bye");
+    }

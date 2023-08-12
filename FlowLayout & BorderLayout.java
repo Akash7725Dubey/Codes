a72db378d498b60ac12ca8f7104d454d60ae60bf
Akash7725Dubey <140379436+Akash7725Dubey@users.noreply.github.com>
2023-08-12 11:21:17 +0530

    Add files via upload

---
+++ FlowLayout & BorderLayout.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.awt.event.*;
+class MyFrame extends Frame
+{
+    Button b1,b2,b3,b4,b5,b6;
+    public MyFrame()
+    {
+        super("FlowLayout Demo");
+        b1=new Button("One");
+        b2=new Button("Two");
+        b3=new Button("Three");
+        b4=new Button("Four");
+        b5=new Button("Five");
+        b6=new Button("Six");
+
+        add(b1,BorderLayout.NORTH);
+        add(b2,BorderLayout.SOUTH);
+        add(b3,BorderLayout.EAST);
+        add(b4,BorderLayout.WEST);
+        add(b5,BorderLayout.CENTER);
+        Panel p=new Panel();
+        p.add(new Button("Mon"));
+        p.add(new Button("Tue"));
+        p.add(new Button("Wed"));
+       add(p,BorderLayout.EAST);
+        //add(b6);
+    }
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        MyFrame f=new MyFrame();
+        f.setSize(300,300);
+        f.setVisible(true);
+
+    }
+}
